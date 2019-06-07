1c470904a6e3a7351b377748e8f10c58be54ab5f
kirkiki <ronanpsf@gmail.com>
2019-06-07 12:25:25 +0200

    portal

---
+++ Assets/ronan/portailManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portailManager : MonoBehaviour
+{
+    public GameObject portail1;
+    public GameObject portail2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    GameObject getOtherPort(GameObject _portail)
+    {
+        return (_portail == portail1 ? portail2 : portail1);
+    }
+     void tp(GameObject portailToGo)
+    {
+        portailToGo.GetComponent<Transform>().position = new Vector3(portailToGo.GetComponent<Transform>().position.x + 1, portailToGo.GetComponent<Transform>().position.y + 1, getOtherPort(portailToGo).GetComponent<Transform>().position.z);
+        Vector2 CurrentVelocity = portailToGo.GetComponent<Rigidbody2D>().velocity;
+        float YVelocity = Mathf.Abs(CurrentVelocity.y);
+        CurrentVelocity.y = 0;
+        portailToGo.GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
+        portailToGo.GetComponent<Rigidbody2D>().AddForce(portailToGo.transform.up * YVelocity, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/ronan/portailScr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portailScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject otherPortail;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Transform>().position = new Vector3(otherPortail.GetComponent<Transform>().position.x + 1, otherPortail.GetComponent<Transform>().position.y + 1, otherPortail.GetComponent<Transform>().position.z);
+        Vector2 CurrentVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+        float YVelocity = Mathf.Abs(CurrentVelocity.y);
+        CurrentVelocity.y = 0;
+        collision.gameObject.GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(otherPortail.transform.up * YVelocity, ForceMode2D.Impulse);
+    }
+}

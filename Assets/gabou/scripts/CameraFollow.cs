717a0d90e791c9bc596e440f62fcce005a37766b
Victor Gabou <victor@gabou.fr>
2019-06-13 05:17:59 +0200

    chores(unity): base working

---
+++ Assets/gabou/scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+
+    private Transform t;
+
+    void Start()
+    {
+        t = target.transform;
+    }
+    
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/gabou/scripts/StandingCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingCheck : MonoBehaviour
+{
+    private PlayerController controller;
+    
+    void Start()
+    {
+        controller = GetComponentInParent<PlayerController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        controller.standing = true;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        controller.standing = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        controller.standing = false;
+    }
+}

538ed674589aed490e98f1b04be45be838297e1f
Victor Gabou <victor@gabou.fr>
2019-06-13 06:07:35 +0200

    feat(traps): add portal reset

---
+++ Assets/gabou/scripts/PortalReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalReset : MonoBehaviour
+{
+    public GameObject portalLeft;
+    public GameObject portalRight;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag.Contains("Player"))
+        {
+            portalLeft.SetActive(false);
+            portalRight.SetActive(false);
+        }
+    }
+}

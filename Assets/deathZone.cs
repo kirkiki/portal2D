b1318e115b9f81d1edf138ca60fbd499aa4e4dc2
Marc-Laurent <15917824+Marc-Laurent@users.noreply.github.com>
2019-06-14 13:46:19 +0200

    Death zone

---
+++ Assets/deathZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathZone : MonoBehaviour
+{
+    public StageManager stage;
+
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider && collider.tag.Contains("Player"))
+        {
+            stage.lost = true;
+        }
+    }
+}

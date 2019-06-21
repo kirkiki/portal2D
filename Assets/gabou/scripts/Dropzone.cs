97ec1c9c2d15901560b086eb12703ced480e0cc3
Victor Gabou <victor@gabou.fr>
2019-06-21 04:23:20 +0200

    feat(traps): add dropzone for ball

---
+++ Assets/gabou/scripts/Dropzone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropzone : MonoBehaviour
+{
+    public bool active = false;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag.Contains("Ball"))
+        {
+            active = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.tag.Contains("Ball"))
+        {
+            active = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag.Contains("Ball"))
+        {
+            active = false;
+        }
+    }
+}

956424a52f41e335a3018fa573d9f46337d357ef
Victor Gabou <victor@gabou.fr>
2019-06-21 03:21:33 +0200

    feat(stages): add stage 9 base

--- Assets/gabou/scripts/GameManager.cs
+++ Assets/gabou/scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    static public int stageCount = 8;
+    static public int stageCount = 9;
     static public int stageUnlock = 1;
 
     public GameObject levelsScreen;

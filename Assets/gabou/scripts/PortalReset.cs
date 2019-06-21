65b8a0c12b690a22f8f4f189653dedc7522858d5
Victor Gabou <victor@gabou.fr>
2019-06-21 04:42:47 +0200

    feat(portal reset): link to dropzone to deactivate / activate

--- Assets/gabou/scripts/PortalReset.cs
+++ Assets/gabou/scripts/PortalReset.cs
@@ -6,6 +6,38 @@ public class PortalReset : MonoBehaviour
 {
     public GameObject portalLeft;
     public GameObject portalRight;
+    public GameObject trigger;
+
+    private Dropzone dropzone;
+    private SpriteRenderer r;
+    private BoxCollider2D c;
+
+    void Start()
+    {
+        r = GetComponent<SpriteRenderer>();
+        c = GetComponent<BoxCollider2D>();
+
+        if (trigger)
+        {
+            dropzone = trigger.GetComponent<Dropzone>();
+        }
+        else
+        {
+            dropzone = null;
+        }
+    }
+
+    void Update()
+    {
+        if (dropzone && dropzone.isActive)
+        {
+            Deactivate();
+        }
+        else
+        {
+            Activate();
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -14,5 +46,17 @@ public class PortalReset : MonoBehaviour
             portalLeft.SetActive(false);
             portalRight.SetActive(false);
         }
+    }
+
+    private void Deactivate()
+    {
+        r.enabled = false;
+        c.enabled = false;
+    }
+
+    private void Activate()
+    {
+        r.enabled = true;
+        c.enabled = true;
     }
 }
